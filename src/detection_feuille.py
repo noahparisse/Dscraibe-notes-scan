ebc29507dc75b0fd66f1943c7aabbccef1eee4e0
acorrard <acorrard@gmail.com>
2025-09-24 11:14:48 +0200

    Détection des quadrilatères (et non de tous les types de contours)

--- src/detection_feuille.py
+++ src/detection_feuille.py
@@ -1,9 +1,12 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
+
+# Prétraitement de l'image pour faciliter la détection de contours
 
 def processed_image(img):
     # Convertir en niveaux de gris
@@ -14,22 +17,38 @@ def processed_image(img):
     return blur
 
 
-while True:
-    ret, img = cap.read()
-    processed_img = processed_image(img)
+# Détection des contours et filtrage des quadrilatères
 
+def detect_paper(img):
+    proc = processed_image(img)
     # Détection des bords
-    edges = cv2.Canny(processed_img, 50, 150)
-
+    edges = cv2.Canny(proc, 50, 150)
     # Trouver des contours
     contours, _ = cv2.findContours(
         edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    # Dessiner les contours sur l'image originale
-    img_with_contours = img.copy()
-    cv2.drawContours(img_with_contours, contours, -1, (0, 255, 0), 2)
+    candidates = []
+    for cnt in contours:
+        epsilon = 0.02 * cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, epsilon, True)
 
-    cv2.imshow('Webcam', img_with_contours)
+        # On ne garde que les quadrilatères assez grands
+        if len(approx) == 4 and cv2.contourArea(cnt) > 1000:
+            candidates.append(approx)
+    return candidates
+
+
+# Lancement de la webcam
+while True:
+    ret, img = cap.read()
+    processed_img = processed_image(img)
+
+    possible_papers = detect_paper(img)
+    img_show = img.copy()
+
+    # Dessiner les contours sur l'image originale
+    cv2.drawContours(img_show, possible_papers, -1, (0, 255, 0), 2)
+    cv2.imshow('Webcam', img_show)
 
     if cv2.waitKey(1) == ord('q'):
         break
