5994ee0a84911f424fce030decfdbc6bc3e57e49
Noah <noah.parisse@student-cs.fr>
2025-10-09 15:27:14 +0200

    ajout du script de segmentation pour YOLO detect

--- src/proc/paper_detection/segmentation.py
+++ src/proc/paper_detection/segmentation.py
@@ -5,6 +5,25 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+def get_mask(img, rect):
+    mask = np.zeros(img.shape[:2],np.uint8)
+    bgdModel = np.zeros((1,65),np.float64)
+    fgdModel = np.zeros((1,65),np.float64)
+    cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+
+    # # Raffinage en rensignant les pixels du centr de l'image comme appartanant pour sûr à l'objet
+    # height, width = img.shape[:2]
+    # center_x = int(width / 2)
+    # center_y = int(height / 2)
+    # mask[center_x, center_y] = 3
+    # mask[center_x, center_y+1] = 3
+    # mask[center_x+1, center_y] = 3
+    # mask[center_x+1, center_y+1] = 3
+    # cv2.grabCut(img, mask, None, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_MASK)
+
+    mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
+    return mask2
+
 def search_and_print_object(img, rect):
     mask = np.zeros(img.shape[:2],np.uint8)
     # height, width = img.shape[:2]
@@ -24,26 +43,27 @@ def search_and_print_object(img, rect):
     plt.colorbar()
     plt.show()
 
-files = os.listdir(os.path.join(BASE_DIR, "../../../tmp/"))
-tmp_dir = os.path.join(BASE_DIR, "../../../tmp/")
+if __name__ == "__main__":
+    files = os.listdir(os.path.join(BASE_DIR, "../../../tmp/"))
+    tmp_dir = os.path.join(BASE_DIR, "../../../tmp/")
 
-for f in files:
-    if f.endswith(".jpg"):
-        print("L'image traitée est :", f)
-        img = cv2.imread(os.path.join(tmp_dir, f))
-        height, width = img.shape[:2]
-        stamp = f[6:-4]
-        textfilename = os.path.join(tmp_dir, f"output_{stamp}.txt")
-        with open(textfilename, "r") as t:
-            for ligne in t:
-                valeurs = ligne.strip().split()  # découpe la ligne
-                classe, x, y, w, h = valeurs
-                break
-        box_x_center = int(float(x)*width)
-        box_y_center = int(float(y)*height)
-        box_width = int(float(w)*width)
-        box_height = int(float(h)*height)
-        box_x_left = int(box_x_center-0.5*box_width)
-        box_y_top = int(box_y_center-0.5*box_height)
-        rect = (box_x_left, box_y_top, box_width, box_height)
-        search_and_print_object(img, rect)
+    for f in files:
+        if f.endswith(".jpg"):
+            print("L'image traitée est :", f)
+            img = cv2.imread(os.path.join(tmp_dir, f))
+            height, width = img.shape[:2]
+            stamp = f[6:-4]
+            textfilename = os.path.join(tmp_dir, f"output_{stamp}.txt")
+            with open(textfilename, "r") as t:
+                for ligne in t:
+                    valeurs = ligne.strip().split()  # découpe la ligne
+                    classe, x, y, w, h = valeurs
+                    break
+            box_x_center = int(float(x)*width)
+            box_y_center = int(float(y)*height)
+            box_width = int(float(w)*width)
+            box_height = int(float(h)*height)
+            box_x_left = int(box_x_center-0.5*box_width)
+            box_y_top = int(box_y_center-0.5*box_height)
+            rect = (box_x_left, box_y_top, box_width, box_height)
+            search_and_print_object(img, rect)
